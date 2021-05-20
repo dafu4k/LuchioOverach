3c2e385ac33da4dd7d4f25acdd1483c2f1dc5977
Ilya <dafu4k@gmail.com>
2021-05-20 16:08:02 +0300

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const namealert = "Roma";
+const surname = "Cipapa";
+
+console.log(`Твоё имя - ${namealert}, а фамилия - ${surname}`);
+
+let first = 7,
+    second = 8;
+
+console.log(first++);
+console.log(++first);
+
+console.log(2 + 2 !== 4);
+
+
+
+
+
