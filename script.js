6d902556e1a3c83b367457626b429546fad15b76
dafu4k <dafu4k@gmail.com>
2021-06-17 15:46:10 +0300

    17.06.2021

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-/*
-const NumberOfFilms = prompt("Сколько фильмов вы посмотрели?"," ");
+
+/*const NumberOfFilms = prompt("Сколько фильмов вы посмотрели?"," ");
 const PersonalMovieDB = {
     count: NumberOfFilms,
     movies: {
@@ -14,18 +14,31 @@ const PersonalMovieDB = {
     privat: false
 };
 
-const qs = prompt("Какой твой последний фильм?", ""),
-    qs1 = prompt("На сколько баллов ты его оценишь?", ""),
-    qs2 = prompt("Какой твой второй последний фильм?", ""),
-    qs3 = prompt("На сколько баллов ты его оценишь?", "");
+
+for (let i = 0; i < 2;i++){
+    const a = prompt("Какой твой последний фильм?", ""),
+          b = prompt("На сколько баллов ты его оценишь?", "");
+          if (a.length == 0 || a.length > 50 || a == null ){
+            alert("Учтите, что длина поля не должна быть больше 50 символов, меньше одного или вообще иметь пустую строку");
+            i--;                                                /*Можно сказать заставляет нас заново отвечать на тот же самый вопрос
+                                                                За счёт того, что мы "отклоняем" шаг в цикле*//*  
+          } else {
+              PersonalMovieDB.movies[a] = b;
+              console.log(PersonalMovieDB.movies);
+          }
+        }
 
 
-PersonalMovieDB.movies[qs]=  qs1;
-PersonalMovieDB.movies[qs2] = qs3;
-console.log(PersonalMovieDB.movies);
-document.write(PersonalMovieDB.movies);
+if ((PersonalMovieDB.count) <= 10){
+    alert("Как-то мало фильмов вы смотрите!");
+} else if ((PersonalMovieDB.count) > 10 && (PersonalMovieDB.count) <= 30){
+    alert("Хочу заметить, что вы не безразличны к фильмам!");
+} else {
+    alert("Да вы самый настоящий киноман!");
+}
+/*         
 
-*/
+
 
 
 
--- second.js
+++ second.js
@@ -1,1 +1,14 @@
 "use strict";
+/*
+ES 6
+Условия
+(da !== 10) ? console.log("Не равен") : console.log("Равен");
+
+Функций
+const a = (a, b) => a+b;
+console.log(a(5,5));
+*/
+
+
+
+
