6c5ba94b3f5570af42ca8dbf66036dc1b389514b
dafu4k <dafu4k@gmail.com>
2021-07-01 22:26:32 +0300

    1.07.2021

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 "use strict";
 
 
-/*const NumberOfFilms = prompt("Сколько фильмов вы посмотрели?"," ");
+/*let NumberOfFilms;
+
+function start() {                                                                   //В функциях не забывать ()!!!!
+    NumberOfFilms = +prompt("Сколько фильмов вы посмотрели?"," ");
+
+    while (NumberOfFilms == null || NumberOfFilms == " " || isNaN(NumberOfFilms)) {
+        NumberOfFilms = prompt("Сколько фильмов вы посмотрели?"," ");
+    }
+    return NumberOfFilms;
+}
+start();
+
+
 const PersonalMovieDB = {
     count: NumberOfFilms,
     movies: {
@@ -15,25 +27,55 @@ const PersonalMovieDB = {
 };
 
 
-for (let i = 0; i < 2;i++){
-    const a = prompt("Какой твой последний фильм?", ""),
-          b = prompt("На сколько баллов ты его оценишь?", "");
-          if (a.length == 0 || a.length > 50 || a == null ){
-            alert("Учтите, что длина поля не должна быть больше 50 символов, меньше одного или вообще иметь пустую строку");
-            i--;                                                /*Можно сказать заставляет нас заново отвечать на тот же самый вопрос
-                                                                За счёт того, что мы "отклоняем" шаг в цикле*//*  
-          } else {
-              PersonalMovieDB.movies[a] = b;
-              console.log(PersonalMovieDB.movies);
-          }
-        }
-
-
-if ((PersonalMovieDB.count) <= 10){
-    alert("Как-то мало фильмов вы смотрите!");
-} else if ((PersonalMovieDB.count) > 10 && (PersonalMovieDB.count) <= 30){
-    alert("Хочу заметить, что вы не безразличны к фильмам!");
-} else {
-    alert("Да вы самый настоящий киноман!");
+function rememberMyFilms(){
+    for (let i = 0; i < 2;i++){
+        const a = prompt("Какой твой последний фильм?", ""),
+              b = prompt("На сколько баллов ты его оценишь?", "");
+              if (a.length == 0 || a.length > 50 || a == null ){
+                alert("Учтите, что длина поля не должна быть больше 50 символов, меньше одного или вообще иметь пустую строку");
+                i--;                                                /*Можно сказать заставляет нас заново отвечать на тот же самый вопрос
+                                                                    За счёт того, что мы "отклоняем" шаг в цикле*/  
+             /* } else {
+                  PersonalMovieDB.movies[a] = b;
+                  console.log(PersonalMovieDB.movies);
+              }
+            }
 }
-/*         
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if ((PersonalMovieDB.count) <= 10){
+        alert("Как-то мало фильмов вы смотрите!");
+    } else if ((PersonalMovieDB.count) > 10 && (PersonalMovieDB.count) <= 30){
+        alert("Хочу заметить, что вы не безразличны к фильмам!");
+    } else {
+        alert("Да вы самый настоящий киноман!");
+    }
+}
+
+detectPersonalLevel();
+
+
+function showMyDB() {
+    if (PersonalMovieDB.privat == false){
+        console.log(PersonalMovieDB);
+    }
+}
+
+showMyDB();
+
+
+function writeYourGenres() {
+    for (let i = 1; i != 4;i++){
+        let a = prompt(`Ваш любимый жанр под номером ${i}`);
+        PersonalMovieDB.genres[i] = a;
+    }
+}
+
+writeYourGenres();
+
+console.log(PersonalMovieDB);
+*/  
+
+
--- second.js
+++ second.js
@@ -9,7 +9,7 @@ const a = (a, b) => a+b;
 console.log(a(5,5));
 */
 
-const a = prompt("Андрей хуй","");
+/*const a = prompt("Андрей хуй","");
 
 
 const b = a.indexOf(" ");
@@ -18,4 +18,88 @@ const c = a.length;
 console.log(a.slice(b+1,c+1));
 
 
+
+
+function kavo() {
+
+}
+
+
+const tavo = function(){
+
+};
+
+const da = (a,b) => a+b;
+
+console.log(da(2,2));
+
+
+
+if (a>b){
+
+} else if(a<b || a == 0){
+
+}
+
+(a==b) ? console.log(a) : console.log(b);
+
+
+switch (a){
+    case 8 :
+}
+
+
+while (i>b){
+
+}
+
+
+for (let net = 1; net != 4; net++){
+    
+}
+*/
+
+
+
+
+
+
+/*for (let key in obj){
+    if (typeof(obj[key]) === "object"){
+        for (let i in obj[key]){
+            console.log(`Це уже объекст с ключём ${i} и значением ${obj[key][i]}`);
+        }
+    } else{
+        console.log(`Это свойство имеет ключ под названием ${key} и значение в нём - ${obj[key]}`);
+    }
+}
+*/
+
+/*const array = [4, 5, 6, 7, 8];
+
+array.forEach(function(a, b){
+    console.log(`a = ${a}`);
+    console.log(`b = ${b}`);
+});*/
+
+let a = prompt("", "");
+a = a.split(",") || a.split(", ") || a.split("; "); // Из а получаем что-то
+
+function da(){
+   for (let value of a){
+       if (typeof(value) === "int"){
+           a.sort(Num);
+       } else{
+           a.sort();
+       }
+   }
+   return a;
+}
+da();
+console.log(a);
+function Num(a,b){
+    return a - b;
+} 
+
+ 
 
